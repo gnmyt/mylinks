99ca1a11d661748af6f8faac7cca941bbd43738f
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-27 15:20:40 +0100

    Created the TextBox.jsx

---
+++ admin/src/common/components/TextBox/TextBox.jsx
@@ -0,0 +1,14 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "./styles.sass";
+
+export const TextBox = (props) => {
+    const handleAnimation = () => props.useRef ? props.useRef.current.classList.value = "textbox-wrapper" : "";
+
+    return (
+        <div className="textbox-wrapper" ref={props.useRef} onAnimationEnd={handleAnimation}>
+            <input className="textbox-input" placeholder={props.placeholder} value={props.value}
+                   onChange={props.onUpdate} type={props.type || "text"} autoComplete={props.autoComplete}/>
+            <FontAwesomeIcon icon={props.icon} className="textbox-icon"/>
+        </div>
+    );
+}
