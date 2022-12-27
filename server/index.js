10d7a44b4a37121d493f3a996efb278472bdd9f6
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-27 15:09:28 +0100

    Created the server index.js

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const db = require('./config/database');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5217;
+
+app.set('view engine', 'ejs');
+
+// Default middleware
+app.use(express.json());
+
+// Middlewares which require no authentication
+app.use("/api/auth", require('./routes/auth'));
+
+// Middlewares which require authentication
+app.use("/api*", require('./middlewares/authenticate'));
+app.use("/api/link", require('./routes/link'));
+app.use("/api/info", require('./routes/info'));
+
+app.use("/api*", (req, res) => res.json({message: "Route not found"}));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use("/admin", express.static(path.join(__dirname, "../admin/dist")));
+
+    app.get("/admin*", (req, res) => res.sendFile(path.join(__dirname, '../admin/dist', 'index.html')));
+} else {
+    app.get("/admin*", (req, res) => res.redirect("/"));
+}
+
+app.use("*", require('./actions/click'));
+
+const run = async () => {
+    require('./controller/token').createToken();
+    require('./controller/module').initialize();
+
+    await db.sync({alter: true, force: false});
+
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+}
+
+db.authenticate().then(() => {
+   console.log("Successfully connected to the database");
+   run();
+}).catch(err => {
+    console.error("Could not connect to the database: " + err.message);
+    process.exit(111);
+});
