6930654e0cdd7ef09b717d1e1258cb2730329670
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-27 15:07:58 +0100

    Created the link validation

---
+++ server/validations/link.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+module.exports.listLinksValidation = Joi.object({
+    creator: Joi.string().min(5).max(25),
+    tags: Joi.string().max(50),
+    title: Joi.string().min(3).max(25),
+    limit: Joi.number().min(1).max(5000)
+});
+
+module.exports.createLinkValidation = Joi.object({
+    accessId: Joi.string().min(3).max(25),
+    domainName: Joi.string().min(2).max(55).required(),
+    title: Joi.string().min(3).max(25),
+    type: Joi.string().max(25).required(),
+    tags: Joi.array().max(5),
+    meta: Joi.object().required()
+});
