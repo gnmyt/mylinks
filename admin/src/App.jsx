6c85ff5bcc851c2e3ebd0618253588ad3f726aa8
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-28 03:40:13 +0100

    Imported all providers into the App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,24 @@
 import {LoginProvider} from "@/common/contexts/Login";
 import "@/common/styles/fonts.sass";
 import './App.sass';
+import {LinkProvider} from "@/common/contexts/Link";
 import Home from "@/pages/Home";
+import {ModuleProvider} from "@/common/contexts/Module";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import {UserProvider} from "@/common/contexts/User";
+
+library.add(fas);
 
 const App = () => (
     <LoginProvider>
-        <Home/>
+        <UserProvider>
+            <ModuleProvider>
+                <LinkProvider>
+                    <Home/>
+                </LinkProvider>
+            </ModuleProvider>
+        </UserProvider>
     </LoginProvider>
 )
 
