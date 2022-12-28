c4cdd422e8876d84ac59e96caa15600768ee3c1b
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-28 03:39:54 +0100

    The "Link erstellen" button in the ManageArea.jsx now loads the default link module

--- admin/src/pages/Home/components/ManageArea/ManageArea.jsx
+++ admin/src/pages/Home/components/ManageArea/ManageArea.jsx
@@ -2,12 +2,18 @@ import "./styles.sass";
 import TextBox from "@/common/components/TextBox";
 import Button from "@/common/components/Button";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import LinkDialog from "@/pages/Home/components/LinkDialog";
+import {useState} from "react";
 
 export const ManageArea = () => {
+
+    const [showDialog, setShowDialog] = useState(false);
+
     return (
         <div className="manage-area">
+            <LinkDialog module="link" isOpen={showDialog} close={() => setShowDialog(false)}/>
             <TextBox icon={faSearch} placeholder="Link suchen"/>
-            <Button text="Link erstellen" />
+            <Button text="Link erstellen" onClick={() => setShowDialog(true)} />
         </div>
     )
 }
