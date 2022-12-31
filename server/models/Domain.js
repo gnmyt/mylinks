d2df47663dbe6435de08aef6d3b9db9daab74d1b
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-31 22:41:54 +0100

    Made the domainName the primary key in the Domain model

--- server/models/Domain.js
+++ server/models/Domain.js
@@ -4,6 +4,7 @@ const db = require("../config/database");
 module.exports = db.define("domains", {
     domainName: {
         type: Sequelize.STRING,
+        primaryKey: true,
         allowNull: false
     }
 });
