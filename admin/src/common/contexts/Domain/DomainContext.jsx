2c948d5bfa439cf82e4adeaa602142c967edaf66
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-01 04:22:34 +0100

    Created the DomainContext.jsx

---
+++ admin/src/common/contexts/Domain/DomainContext.jsx
@@ -0,0 +1,33 @@
+import React, {createContext, useEffect, useState} from "react";
+import {jsonRequest} from "@/common/utils/RequestUtil";
+
+export const DomainContext = createContext({});
+
+export const DomainProvider = (props) => {
+
+    const [currentDomain, setCurrentDomain] = useState(localStorage.getItem("domain") || window.location.host);
+    const [domains, setDomains] = useState([]);
+
+    const updateDomains = () => jsonRequest(`/domains`)
+        .then(json => setDomains(json));
+
+    const updateCurrentDomain = (domain) => {
+        setCurrentDomain(domain);
+        localStorage.setItem("domain", domain);
+    }
+
+    useEffect(() => {
+        updateDomains();
+    }, []);
+
+    useEffect(() => {
+        if (!domains.includes(currentDomain))
+            updateCurrentDomain(domains.length !== 0 ? domains[0] : window.location.host);
+    }, [domains]);
+
+    return (
+        <DomainContext.Provider value={[currentDomain, updateCurrentDomain, domains, updateDomains]}>
+            {props.children}
+        </DomainContext.Provider>
+    )
+}
