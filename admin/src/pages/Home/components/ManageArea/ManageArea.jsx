635280c94fcd7f4a699d5b4bc2adab44c971a34c
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-01 04:36:23 +0100

    Removed hard coded statements from the ManageArea.jsx & integrated the DomainButton.jsx

--- admin/src/pages/Home/components/ManageArea/ManageArea.jsx
+++ admin/src/pages/Home/components/ManageArea/ManageArea.jsx
@@ -1,19 +1,22 @@
-import "./styles.sass";
 import TextBox from "@/common/components/TextBox";
 import Button from "@/common/components/Button";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
-import LinkDialog from "@/pages/Home/components/LinkDialog";
 import {useState} from "react";
+import LinkChooser from "@/pages/Home/components/LinkChooser";
+import DomainButton from "@/pages/Home/components/ManageArea/components/DomainButton";
+import "./styles.sass";
 
 export const ManageArea = () => {
-
     const [showDialog, setShowDialog] = useState(false);
 
     return (
         <div className="manage-area">
-            <LinkDialog module="link" isOpen={showDialog} close={() => setShowDialog(false)}/>
+            <LinkChooser isOpen={showDialog} close={() => setShowDialog(false)}/>
             <TextBox icon={faSearch} placeholder="Link suchen"/>
-            <Button text="Link erstellen" onClick={() => setShowDialog(true)} />
+            <div className="right-area">
+                <DomainButton />
+                <Button text="Link erstellen" onClick={() => setShowDialog(true)} />
+            </div>
         </div>
     )
 }
