a52ef16889f1edc5565ecad0df5bd4663bb066c9
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-01 04:27:50 +0100

    Added the closing option to the DialogHeader.jsx

--- admin/src/pages/Home/components/LinkDialog/components/DialogHeader/DialogHeader.jsx
+++ admin/src/pages/Home/components/LinkDialog/components/DialogHeader/DialogHeader.jsx
@@ -1,18 +1,24 @@
-import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faClose} from "@fortawesome/free-solid-svg-icons";
+import {useContext} from "react";
+import DialogContext from "@/common/contexts/Dialog";
+import "./styles.sass";
 
-export const DialogHeader = (props) => (
-    <div className="dialog-header">
-        <div className="info-area">
-            <div className="module-info">
-                <FontAwesomeIcon className="info-icon" icon={["fas", props.icon]}/>
+export const DialogHeader = (props) => {
+    const close = useContext(DialogContext);
+
+    return (
+        <div className="dialog-header">
+            <div className="info-area">
+                <div className="module-info">
+                    <FontAwesomeIcon className="info-icon" icon={["fas", props.icon]}/>
+                </div>
+                <div className="text-area">
+                    <h2>{props.editMode ? "Link bearbeiten" : "Neuer Link"}</h2>
+                    <p>{props.module}</p>
+                </div>
             </div>
-            <div className="text-area">
-                <h2>Neuer Link</h2>
-                <p>{props.module}</p>
-            </div>
+            <FontAwesomeIcon className="close-icon" icon={faClose} onClick={close}/>
         </div>
-        <FontAwesomeIcon className="close-icon" icon={faClose} onClick={props.close}/>
-    </div>
-)
+    )
+}
