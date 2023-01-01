6c5e4b8998061df49633ee979a708d6e06791b49
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-01 20:43:23 +0100

    Made the Tag.jsx clickable

--- admin/src/pages/Home/components/Tag/Tag.jsx
+++ admin/src/pages/Home/components/Tag/Tag.jsx
@@ -1,10 +1,21 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTag} from "@fortawesome/free-solid-svg-icons";
+import {useContext} from "react";
+import LinkContext from "@/common/contexts/Link/index.js";
 import "./styles.sass";
 
-export const Tag = (props) => (
-    <div className="tag">
-        <FontAwesomeIcon icon={faTag} />
-        <h3>{props.name}</h3>
-    </div>
-)
+export const Tag = (props) => {
+    const [links, updateLinks, setQuery, query, search, setSearch] = useContext(LinkContext);
+
+    const addQuery = () => {
+        if (!query.tag?.split(",").includes(props.name))
+            setSearch(`tag:"${props.name}" ${search}`);
+    }
+
+    return (
+        <div className="tag" onClick={addQuery}>
+            <FontAwesomeIcon icon={faTag}/>
+            <h3>{props.name}</h3>
+        </div>
+    )
+}
