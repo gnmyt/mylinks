8cdf6fca6c201fc64895b093bbee98920b8c791a
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-08 03:27:47 +0100

    Created the redirect module

---
+++ modules/redirect/module.js
@@ -0,0 +1,31 @@
+const Joi = require("joi");
+
+module.exports.onClick = (id, accessId, metaData, controller) => {
+    controller.locals.meta.timer = controller.locals.meta.timer || 5;
+    controller.render(__dirname + "/web/index");
+}
+
+module.exports.info = {
+    name: "Nutzer weiterleiten",
+    icon: "compass",
+    validationSchema: Joi.object({
+        shortenUrl: Joi.string().max(2500).uri().required(),
+        timer: Joi.number().min(1).max(60)
+    }),
+    meta: [{
+        name: "Link-Einstellungen",
+        type: "default",
+        fields: {
+            shortenUrl: {
+                type: "text",
+                name: "Zu kürzende URL",
+                description: "Die zu kürzende (lange) URL"
+            },
+            timer: {
+                type: "number",
+                name: "Weiterleitung in Sekunden",
+                description: "Wann soll weitergeleitet werden?"
+            }
+        }
+    }]
+}
